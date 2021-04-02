6b508e6f13466bc86f4f303e3276ee298e6a6078
Lo-GR <diamondintheroth@gmail.com>
2021-04-02 15:12:52 -0700

    add swagger to project

--- NationalParksApi/Startup.cs
+++ NationalParksApi/Startup.cs
@@ -24,11 +24,17 @@ namespace NationalParksAPI
             services.AddDbContext<NationalParksAPIContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
