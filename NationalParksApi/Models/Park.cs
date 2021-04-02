d4513b601d99d2cd4ad4a6abdc076409b3b89716
Lo-GR <diamondintheroth@gmail.com>
2021-04-02 09:57:35 -0700

    add park model

---
+++ NationalParksApi/Models/Park.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace NationalParksAPI.Models
+{
+  public class Park 
+  {
+    public int ParkId {get; set;}
+    [Required]
+    public string Name {get; set;}
+    [Required]
+    public int Distance {get; set;}
+    [Required]
+    public string Established {get; set;}
+    public string ImageURL {get; set;}
+  }
+}
